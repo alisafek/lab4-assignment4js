4a5e45bdbe17314d9784e0237cad0267ddaca3d4
alisafek <fekla@tlu.ee>
2023-05-03 18:55:32 +0300

    button generation exercise part 2

--- 3-menu.js
+++ 3-menu.js
@@ -71,6 +71,14 @@ const menuList = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "food item x",
+    category: "new",
+    price: 26.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 /* return `<div class="menu-item-col">
@@ -89,7 +97,7 @@ const menuList = [
 return `<button class="filter-btn" type="button" data-id=  >  </button>`
 */
 const sectionCenter = document.querySelector(".menu-wrap-section");
-const filterBtns = document.querySelectorAll(".filter-btn"); //will find all tags with this class selectors
+
 const btnContainer = document.querySelector(".btn-container");
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menuList);
@@ -118,6 +126,26 @@ function displayMenuItems(menuItems) {
 }
 
 function displayMenuButtons() {
+  const categories = menuList.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) values.push(item.category);
+      return values;
+    },
+    ["all"]
+  );
+
+  console.log(categories);
+
+  const categoryBtns = categories
+    .map(function (itemCategory) {
+      return `<button class="filter-btn" type="button" data-id=${itemCategory}  > ${itemCategory}  </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+
+  const filterBtns = document.querySelectorAll(".filter-btn"); //will find all tags with this class selectors
+
   filterBtns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
       // console.log(e.currentTarget.dataset.id);
